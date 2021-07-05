115116f171f0b059f1d21cf15f4622559a64fbe6
Oscar Campaña <descritteriado@Neferet.local>
2021-07-05 13:27:51 -0500

    primer commit

---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html'
+})
+export class ListadoComponent{
+
+  constructor() {
+    
+   }
+
+   heroes: string[]=['Spiderman', 'Hulk', 'Ironman'];
+    heroeBorrado :string =''; 
+   borrarHeroe()
+   {
+ 
+    this.heroeBorrado =  this.heroes.shift()||'';
+
+   
+    
+   }
+}
