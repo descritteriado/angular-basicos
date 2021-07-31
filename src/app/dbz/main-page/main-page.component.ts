5ef34d24440d79752a021767a56f95e89d915056
Oscar Campana <oscar.e.campana@gmail.com>
2021-07-31 10:22:43 -0500

    Fin de ejercicio DBZ

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+
+
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  /*
+  get personajes():Personaje[]{
+    return this.dbzService.personajes;
+  }
+  */
+
+  nuevo: Personaje = {
+    nombre: 'Maestro Roshi',
+    poder: 1000
+  }
+
+  
+  constructor(){
+}
+}
---
+++ src/app/dbz/personajes/personajes.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
+
+@Component({
+  selector: 'app-personajes',
+  templateUrl: './personajes.component.html',
+  styleUrls: ['./personajes.component.css']
+})
+export class PersonajesComponent {
+
+  //@Input('data') personajes: Personaje[]=[];
+
+  get personajes() {
+    return this.dbzServie.personajes;
+  }
+  constructor(private dbzServie: DbzService){
+
+  }
+}
